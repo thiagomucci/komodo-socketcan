c62a32023cc6133bbb8d2c2398fd2c659d3a163c
thiagomucci <thmucc@protonmail.com>
2026-09-05 01:40:40 -0400

    fix: fixed build setup folder layout

--- run_test.sh
+++ run_test.sh
@@ -5,5 +5,5 @@ sudo ip link set up vcan0
 
 gcc -c src/komodo.c -o src/komodo.o -I.
 g++ src/bridge.cpp src/komodo.o -o src/bridge -I. -ldl
-g++ src/test_bridge.cpp -o tests/test_bridge
+g++ tests/test_bridge.cpp -o tests/test_bridge
 echo "Compilation completed tests ready for execution"
